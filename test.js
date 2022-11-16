3e990fcef2226a9ce6fdf9fb47cc575ddb014ec5
Damian <damixd02@gmail.com>
2022-11-16 20:54:57 +0100

    prototype command inputting

--- test.js
+++ test.js
@@ -1,2 +1,27 @@
-console.log("Hello World!");
-console.log("Just to be sure!");
+let strings = {
+    "help": {
+        req: {
+            lvl: 0,
+            login: "",
+        }
+    }
+}
+
+const input_field = document.getElementById("input-field");
+const output_field = document.getElementById("output-field")
+
+const parseMessage = (e) => {
+    var msg = e.target.value;
+    if (msg.includes("\n")) {
+        if (msg.slice(0, -1) in strings) {
+            output_field.value = msg;
+        }
+        else {
+            output_field.value = "Unknown command. Please enter something correct."
+        }
+        e.target.value = "";
+    }
+}
+
+
+input_field.addEventListener("input", parseMessage)
